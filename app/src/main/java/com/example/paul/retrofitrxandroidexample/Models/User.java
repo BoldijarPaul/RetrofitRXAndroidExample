a3681811507b8fbb6fbba14a956fcef2689235ca
Boldijar Paul <paul.bv96@yahoo.com>
2015-10-12 12:42:43 +0300

    added user

---
+++ app/src/main/java/com/example/paul/retrofitrxandroidexample/Models/User.java
@@ -0,0 +1,53 @@
+package com.example.paul.retrofitrxandroidexample.Models;
+
+/**
+ * Created by Paul on 12/10/15.
+ */
+public class User {
+
+    private String login;
+    private long id;
+    private String name;
+    private String company;
+    private String location;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
