9f3c6a49905eb0b3fd675947f2af48c1d96b913b
Boldijar Paul <paul.bv96@yahoo.com>
2015-10-12 12:48:38 +0300

    added GithubServiceApi

---
+++ app/src/main/java/com/example/paul/retrofitrxandroidexample/retrofit/GithubServiceApi.java
@@ -0,0 +1,15 @@
+package com.example.paul.retrofitrxandroidexample.retrofit;
+
+import com.example.paul.retrofitrxandroidexample.Models.User;
+
+import retrofit.http.GET;
+import retrofit.http.Path;
+
+/**
+ * Created by Paul on 12/10/15.
+ */
+public interface GithubServiceApi {
+
+    @GET("/users/{login}")
+    public User getUserByLogin(@Path("login") String login);
+}
